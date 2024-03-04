413b8c113ef5ec51e75af2d9bf3b4f6af26c0433
Arctis-Store <161778372+Arctis-Store@users.noreply.github.com>
2024-03-04 19:25:04 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,11 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Arctis',
+    description: 'Arctis Store Documentation',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      discord: 'dc.arctis-store.pl',
+      github: 'Arctis-Store/arctis-store-docs',
     },
     github: {
       dir: '.starters/default/content',
@@ -33,6 +28,16 @@ export default defineAppConfig({
       showLinkIcon: true,
       exclude: [],
       fluid: true
-    }
+    },
+    footer: {
+      credits: {
+        icon: 'https://cdn.discordapp.com/attachments/1206034530432196700/1213190963133157416/logostore.png?ex=65f49321&is=65e21e21&hm=6e7fdb444620cbfccbd1b5ef03702e813e99ebda277e756c2f0c4ae1a329bfc1&',
+        text: 'Arctis Store',
+        href: 'https://arctis-store.pl'
+      },
+      textLinks: [],
+      iconLinks: [],
+      fluid: false
+    },
   }
 })
