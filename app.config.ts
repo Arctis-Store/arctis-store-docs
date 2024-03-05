0dfc05cdee103d7e23297b73ea5c9b28dbdc478a
Arctis-Store <161778372+Arctis-Store@users.noreply.github.com>
2024-03-05 04:45:54 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default defineAppConfig({
     description: 'Arctis Store Documentation',
     image: 'https://cdn.discordapp.com/attachments/1206034530432196700/1213921870764703886/banner.png?ex=65f73bd7&is=65e4c6d7&hm=f7faed0dbfeb42f62a049294f879b7aafba74b2d1214c3bbd67a17e7a6435f6b&',
     socials: {
-      github: 'Arctis-Store/arctis-store-docs',
+      tebex: 'https://arctis-store.pl',
       ['discord']: {
         label: 'Discord',
         icon: 'ic:baseline-discord',
