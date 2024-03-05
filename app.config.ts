60f9ecfcb4bc692550a94a2fc3cd069882289cee
Arctis-Store <161778372+Arctis-Store@users.noreply.github.com>
2024-03-05 04:35:42 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -5,12 +5,12 @@ export default defineAppConfig({
     image: 'https://cdn.discordapp.com/attachments/1206034530432196700/1213921870764703886/banner.png?ex=65f73bd7&is=65e4c6d7&hm=f7faed0dbfeb42f62a049294f879b7aafba74b2d1214c3bbd67a17e7a6435f6b&',
     socials: {
       github: 'Arctis-Store/arctis-store-docs',
-      youtube: 'https://youtube.com/@ArctisStore',
       ['discord']: {
         label: 'Discord',
-        icon: 'BaselineDiscord',
+        icon: 'ic:baseline-discord',
         href: 'www.dc.arctis-store.pl'
-      }
+      },
+      youtube: 'https://youtube.com/@ArctisStore',
     },
     aside: {
       level: 0,
@@ -29,7 +29,7 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        icon: 'IconDocus',
+        icon: 'tebex',
         text: 'Arctis Store',
         href: 'https://arctis-store.pl'
       },
